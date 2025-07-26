2c885a5b6691abd2cbb2cecc6b3f04f094dc9498
akrem nasir <akynas111@gmail.com>
2025-07-26 17:27:33 +0300

    add the backend packages

--- app.js
+++ app.js
@@ -10,9 +10,11 @@ const PORT = process.env.PORT || 5000;
 
 // Enable CORS
 app.use(cors({
-  origin: "*",
+  origin: "http://localhost:3000",
+  credentials: true,
   exposedHeaders: ["Content-Disposition"]
 }));
+
 
 // Setup Multer for PDF upload
 const upload = multer({ dest: "uploads/" });
